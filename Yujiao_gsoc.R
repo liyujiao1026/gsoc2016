74f5f0bc37cb4fe0dc888e34f0f4a5532f54f28e
Yujiao Li <liyujiao1026@163.com>
2016-03-23 04:08:33 +0100

    Added files via upload

--- Yujiao_gsoc.R
+++ Yujiao_gsoc.R
@@ -1,5 +1,5 @@
 rm(list = ls())
- 
+
 library("spacetime")
 library("sp")
 library("RPostgreSQL")
@@ -50,19 +50,19 @@ dbSendQuery(con, "COMMENT ON SCHEMA yujiao IS 'Test result by YujiaoLi';")
 #==========================   Step 3. Export to the PostgreSQL   =============================================#
 
 
-#--- (3.1) Method 1: Use function "rgdal::writeOGR" to export (Work bad)-------------------|
+#--- (3.1) Method 1: Use function "rgdal::writeOGR" to export (Work bad)-------------------#
 
-# -----------------------------------------------------------------------------------------|
-# *Pro: convenient and fairly straightforward syntax. 							    	                         |
-#											                                                                               |
-# *Con: It can only support several drivers. ( ogrDrivers() to check available drivers)    |
-#       														                                                                     |
-#       Sometimes,Windows-OS cannot work well.                                             |
-#       It depends on the individual user's installation of GDAL/OGR.                      |
-#       "Rgdal" package installed from the CRAN repos includes its own basic gdal,         |
-#       but it doesn't have the necessary driver.                                          |
-# If availabe, use "writeOGR" write into postgis directly. Orelse, consider (3.2) solution |
-#------------------------------------------------------------------------------------------|
+# -----------------------------------------------------------------------------------------#
+# *Pro: convenient and fairly straightforward syntax. 						 
+#											                         
+# *Con: It can only support several drivers. ( ogrDrivers() to check available drivers)    
+#       														 
+#       Sometimes,Windows-OS cannot work well.                                             
+#       It depends on the individual user's installation of GDAL/OGR.                      
+#       "Rgdal" package installed from the CRAN repos includes its own basic gdal,         
+#       but it doesn't have the necessary driver.                                          
+# If availabe, use "writeOGR" write into postgis directly. Orelse, consider (3.2) solution 
+#------------------------------------------------------------------------------------------#
 
 
 if ("PostgreSQL" %in% (ogrDrivers()$name)) {
@@ -89,19 +89,19 @@ file.show(system.file("README.windows", package = "rgdal"))
 
 #---  (3.2) Method 2: Convert spatial object to WKT string, and then export into PostGIS -----------------#
 
-# --------------------------------------------------------------------------------------------------------|
-#  *Pro: Can be generalized to other geometry object, not only limited in spatial-point data	    	        |
-#	   WKT format is rather readable by human than WKB								    				                                      |
-#	   Exported data type(non-sp part) is kept consistent with raw data.No need to set datatype again       |
-#            e.g. Variable "Time" can be recognized and saved as "timestamp with time zone" automatically |
-#        When Converted from WKT to geometry, SRID can be either set well or not. No need to check and set|
-#        Circular Strings and Curves are also supported	                                                  |
-#        "EWKT" can be extended further to allow 3 dimensional data                                       |
-#							                                                                                                  |
-#  *Con: "WKT" is not as faster as "WKB" to process data                                                  |
-#        Not well-wrapped function in the package, this can be done later.                                |
-#  *Note: row.names column was automatically generated.It might be useful for setting primary key. 	      |
-#---------------------------------------------------------------------------------------------------------|
+# --------------------------------------------------------------------------------------------------------#
+#  *Pro: Can be generalized to other geometry object, not only limited in spatial-point data		    
+#	   WKT format is rather readable by human than WKB								    
+#	   Exported data type(non-sp part) is kept consistent with raw data.No need to set datatype again   
+#            e.g. Variable "Time" can be recognized and saved as "timestamp with time zone" automatically 
+#        When Converted from WKT to geometry, SRID can be either set well or not. No need to check and set
+#        Circular Strings and Curves are also supported	                                              
+#        "EWKT" can be extended further to allow 3 dimensional data                                       
+#							                                                                
+#  *Con: "WKT" is not as faster as "WKB" to process data                                                  
+#        Not well-wrapped function in the package, this can be done later.                                
+#  *Note: row.names column was automatically generated.It might be useful for setting primary key. 	    
+#---------------------------------------------------------------------------------------------------------#
 
 wkt <- writeWKT(fires,byid = TRUE)
 class(wkt)  # convert spatial object (coordinates here) to WellKnownText string
@@ -127,10 +127,10 @@ dbSendQuery(con,query)
 
 #---  (3.3) Method 3: Convert spatial object to WKB string,  and then export into PostGIS  ---------------#
 
-# --------------------------------------------------------------------------------------------------------|
-#  *Pro: faster than WKT when data is large, but I need make experiments more times to test               |									 				                                                                                               	|
-#  *my thought: Compared with WKT, WKB is the binary format, I did not see too much difference in SQL     |
-#---------------------------------------------------------------------------------------------------------|
+# --------------------------------------------------------------------------------------------------------#
+#  *Pro: faster than WKT when data is large, but I need make experiments more times to test               														|
+#  *my thought: Compared with WKT, WKB is the binary format, I did not see too much difference in SQL     
+#---------------------------------------------------------------------------------------------------------#
 
 
 wkb <- writeWKB(fires)
@@ -153,24 +153,24 @@ dbSendQuery(con,query)
 
 #---  (3.4) Method 4: Use ST_MakePoint (Manually & rpostgis::pgMakePts)-------------------------------------#
 
-# ----------------------------------------------------------------------------------------------------------|
-#  *Pro: 																	                                                                  |
-#       The function of "pgMakePts" develop the smooth prodecure                                            |
-#          (1)add geometry column:   "ALTER TABLE yujiao.wkbdata ADD COLUMN geom4 geometry(POINT, 32632);"  |
-#          (2)add index on geometry: "CREATE INDEX wkbdata_geom4_idx ON yujiao.wkbdata USING GIST (geom4);" |
-#          (3)update geomtry:        "UPDATE yujiao.wkbdata SET geom4=                                      |
-#                                     ST_SetSRID(ST_MakePoint(X, X), 32632);"                               |
-#											                                         					                                    |
-#  *Con: can be only used in "POINT" or "LINESTRING" geometry cases.                                        |
-#        cannot generalize well to other geometry obj: spatial lines, multilines, multipolygons etc.        |
-#																											                                                      |
-#																									                                                       		|
-#  *Note: Bug in package "rpostgis"                                                   											|
-#              Function "pgMakePts()" did not follow the argument of "x" and "y" in its document.           |
-#		   Document states: x and y is the NAME of coordinates, 											                          |
-#                               but it can only accept the VALUE instead of NAME in this function.          |
-#		                                                                          				                			|
-#-----------------------------------------------------------------------------------------------------------|
+# ----------------------------------------------------------------------------------------------------------#
+#  *Pro: 																	
+#       The function of "pgMakePts" develop the smooth prodecure                                            
+#          (1)add geometry column:   "ALTER TABLE yujiao.wkbdata ADD COLUMN geom4 geometry(POINT, 32632);"  
+#          (2)add index on geometry: "CREATE INDEX wkbdata_geom4_idx ON yujiao.wkbdata USING GIST (geom4);" 
+#          (3)update geomtry:        "UPDATE yujiao.wkbdata SET geom4=                                      
+#                                     ST_SetSRID(ST_MakePoint(X, X), 32632);"                               
+#											                                          
+#  *Con: can be only used in "POINT" or "LINESTRING" geometry cases.                                        
+#        cannot generalize well to other geometry obj: spatial lines, multilines, multipolygons etc.        
+#																		
+#																		
+#  *Note: Bug in package "rpostgis" 												
+#              Function "pgMakePts()" did not follow the argument of "x" and "y" in its document.           
+#		   Document states: x and y is the NAME of coordinates, 							
+#                               but it can only accept the VALUE instead of NAME in this function.          
+#		                                                                          				
+#-----------------------------------------------------------------------------------------------------------#
 
 ## transform to data.frame and then add geometry column
 
@@ -190,7 +190,7 @@ dbListTables(con)
 # (3.4.2) use rpostgis::pgMakePts
 
 #----------------------------------------------------------------------------------------------------------#
-# NOTE: There is one error in pgMakePts() function Document								                                 #
+# NOTE: There is one error in pgMakePts() function Document								     #
 # which one is right for argument of x and y?    VALUE of coordinates   v.s.  NAME of coordinates(document)#
 # Official document said it should be NAME, but my test result is VALUE works well. See my following test  #
 #----------------------------------------------------------------------------------------------------------#
@@ -237,20 +237,20 @@ pgIndex(
 )     ## error due to the quotation in source file of package
 
 
-# --------------     Source file of "rpostgis::pgIndex()" updated suggestions --------------#
-#																						                                                |
-# My suggestion is to modify the source file "pgIndex.r"							                      |
-# https://github.com/basille/rpostgis/blob/master/R/pgIndex.r							                  |
-#																						                                                |
-# The error is in line 53 																                                  |
-# Old code:          " (", colname, ");")												                            |
-# Updated code:      " (\"", colname, "\" );")						            		                  |
-#															                                                              |
-# If we use "rpostgis" package, the generated sql statement is automatically: 			        |
-#            str = "CREATE INDEX wktdata_Time_idx ON yujiao.wktdata (Time);			            |
-#            Apparently, the last part should be quoted as ("Time") instead of (Time)	      |
-#															                                                              |
-#-------------------------------------------------------------------------------------------#
+# --------------     Source file of "rpostgis::pgIndex()" updated suggestions ---------------#
+#															
+# My suggestion is to modify the source file "pgIndex.r"					      
+# https://github.com/basille/rpostgis/blob/master/R/pgIndex.r					
+#														
+# The error is in line 53 											
+# Old code:          " (", colname, ");")									
+# Updated code:      " (\"", colname, "\" );")								
+#															
+# If we use "rpostgis" package, the generated sql statement is automatically: 			
+#            str = "CREATE INDEX wktdata_Time_idx ON yujiao.wktdata (Time);			
+#            Apparently, the last part should be quoted as ("Time") instead of (Time)	
+#															
+#--------------------------------------------------------------------------------------------#
 
 
 
